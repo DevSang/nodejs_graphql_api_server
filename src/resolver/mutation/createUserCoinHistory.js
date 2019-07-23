61999b092bf3a40066cc87ef819d143dbed91e70
haeri <haeri@looncup.com>
2019-07-23 17:30:08 +0900

    createUserCoinHistory 수정

--- src/resolver/mutation/createUserCoinHistory.js
+++ src/resolver/mutation/createUserCoinHistory.js
@@ -1,42 +1,39 @@
 const request = require('request-promise-native');
 const moment = require('moment');
 
-module.exports = async (parent, { userId, address, category }, ctx, info) => {
-  console.log('>>[CREATE USER TOKEN HISTORY]');
-  console.log('>>> address : ' + address);
-  console.log('>>> Reward : ' + category);
-  // console.log(category.title);
-  category = category.replace(/'/g, '"');
-  category = JSON.parse(category);
-  const key = Object.keys(category.contents);
-  const value = eval(`category.contents.${key}.coin`);
-  const contents = eval(`category.contents.${key}.text`);
-  console.log('>>> value : ' + value);
-  //url : 172.31.0.13
-  let options = {
-    headers: { 'Content-Type': 'application/json' },
-    url: 'http://34.85.62.36:8080/api/token',
-    body: JSON.stringify({
-      address,
-      value
-    })
-  };
+module.exports = async (parent, { userId, address, category, contents, coin, date}, ctx, info) => {
+   console.log('>>[CREATE USER TOKEN HISTORY]');
+   console.log('>>> address : ' + address);
+   console.log('>>> category : ' + category);
+   console.log('>>> contents : ' + contents);
+   console.log('>>> value : ' + coin);
 
-  let res = await request.post(options);
-  res = JSON.parse(res);
-  let createUserCoin = await ctx.db.mutation.createUser_coin_history({
-    data: {
-      user: {
-        connect: {
-          id: userId
-        }
-      },
-      category: 'REWARDS',
-      contents,
-      coin: value,
-      date: moment()
-      // txHash: res.message
-    }
-  });
-  return createUserCoin;
+   //url : 172.31.0.13
+
+   let options = {
+       headers: { 'Content-Type': 'application/json' },
+       url: 'http://34.85.62.36:8080/api/token',
+       body: JSON.stringify({
+           address,
+           value:coin
+       })
+   };
+
+   let res = await request.post(options);
+   res = JSON.parse(res);
+   let createUserCoin = await ctx.db.mutation.createUser_coin_history({
+       data: {
+           user: {
+               connect: {
+                   id: userId
+               }
+           },
+           category: category,
+           contents: contents,
+           coin: coin,
+           date: date
+           // txHash: res.message
+       }
+   });
+   return createUserCoin;
 };
