7c2a4a828b489c860bf7d79e8432d2ccb4dd0bca
haeri <haeri@looncup.com>
2019-08-09 13:01:20 +0900

    loon ai host url 수정

--- src/resolver/mutation/createTxUserCoinHistory.js
+++ src/resolver/mutation/createTxUserCoinHistory.js
@@ -16,7 +16,8 @@ module.exports = async (
     //url : 172.31.0.13
     let options = {
         headers: { 'Content-Type': 'application/json' },
-        url: 'http://localhost:8080/api/token',
+        // url: 'http://localhost:8080/api/token',
+        url: 'http://35.200.9.86:8080/api/token',
         body: JSON.stringify({
             toAddress: address,
             token: coin,
