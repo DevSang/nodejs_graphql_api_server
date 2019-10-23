268361b205f35918568c2f0cbfd5aa5fb0b6449a
haeri <haeri@looncup.com>
2019-10-23 13:15:00 +0900

    createTxUserCoinHistory address 수정

--- src/resolver/mutation/createTxUserCoinHistory.js
+++ src/resolver/mutation/createTxUserCoinHistory.js
@@ -31,6 +31,8 @@ module.exports = async (
     console.log(`recordedDayCount ${recordedDayCount}`)
     console.log(`isImageColorCount ${isImageColorCount}`)
     let user_id = userId;
+    let dbAddress = address;
+
     // 지불해야할 coin 확인
     if(category == 'REWARDS') {
         let feeQuery = contents.includes('Data record') ? {contents_in: ['RECORD', 'CAMERA RECORD']} : {contents};
@@ -85,6 +87,7 @@ module.exports = async (
                 return null;
             }
             user_id = referralUser[0].id;
+            dbAddress = refferalAddress;
             reqBody.toAddress = refferalAddress;
             reqBody.contents = rewards[0].contents;
             reqBody.token = rewards[0].amount;
@@ -130,7 +133,7 @@ module.exports = async (
         data.coin = data.coin * -1
     }
 
-    let wallet = await ctx.db.query.userWallets({where: {userId: {id: user_id}, address: reqBody.toAddress, status: true}})
+    let wallet = await ctx.db.query.userWallets({where: {userId: {id: user_id}, address: dbAddress, status: true}})
     if(wallet.length !== 0) {
         data.walletId = wallet[0].id
     }
