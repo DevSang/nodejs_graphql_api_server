6915756026101b2fc6b0222b3887743bdec24d23
Steve <steve@looncup.com>
2019-12-06 17:18:27 +0900

    patch -> put

--- src/resolver/mutation/recoverUserWallet.js
+++ src/resolver/mutation/recoverUserWallet.js
@@ -28,7 +28,7 @@ module.exports = async (
         })
     };
     
-    let res = await request.patch(options);
+    let res = await request.put(options);
     await ctx.db.mutation.updateManyUserWallets({where: {userId: {id: Number(userId)}}, data: {status: false}});
     let wallet = await ctx.db.query.userWallets({where: {address}});
     let result;
